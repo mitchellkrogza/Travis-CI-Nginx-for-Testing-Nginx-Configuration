6d7d4650bc0aabadf76a76023872cf1d7bbbbfec
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-01 14:50:17 +0200

    put vhost to listen on port 80 and print out nginx.conf

--- travis-ci/install-nginx2.sh
+++ travis-ci/install-nginx2.sh
@@ -72,7 +72,7 @@ cd /usr/sbin
 #sudo ./setup-ngxblocker -x
 sudo nginx -c /etc/nginx/nginx.conf
 sudo nginx -t &> /dev/stdout
-#cat /etc/nginx/nginx.conf
+cat /etc/nginx/nginx.conf
 #sudo nginx -V
 #sudo netstat -peanut
 #ls -la /etc/nginx/sites-available/

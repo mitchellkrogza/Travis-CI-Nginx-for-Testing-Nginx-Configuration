c38f85d0182344ff455d9d24656e6a1495e8bada
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 13:15:07 +0200

    add sudo to all git commands

--- travis-ci/deploy2.sh
+++ travis-ci/deploy2.sh
@@ -9,9 +9,9 @@ sudo git config --global push.default simple
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 sudo git fetch --tags
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
+if sudo git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
 sudo git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-sudo git push origin master && git push origin master --tags
+sudo git push origin master && sudo git push origin master --tags
 ls -aR
 else echo Tag already exists!; fi
 

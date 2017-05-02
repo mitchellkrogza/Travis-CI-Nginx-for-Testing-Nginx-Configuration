e61042603323f75ce9da79ab461e7738828e8a40
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 15:54:45 +0200

    Remove if statement from tag push

--- travis-ci/deploy2.sh
+++ travis-ci/deploy2.sh
@@ -9,11 +9,11 @@ git config --global push.default simple
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 git fetch --tags
 msg="Tag $GIT_TAG Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
+#if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
 git tag $GIT_TAG -a -m "Tag $GIT_TAG Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push origin master && git push origin master --tags
 ls -aR
-else echo Tag already exists!; fi
+#else echo Tag already exists!; fi
 
 #YEAR=$(date +"%Y")
 #MONTH=$(date +"%m")

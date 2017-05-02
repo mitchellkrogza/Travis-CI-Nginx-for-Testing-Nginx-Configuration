3f74d4d82cd51dcba66e39673aa1f53a44278eeb
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 13:32:13 +0200

    try take ownership of $TRAVIS_BUILD_DIR

--- travis-ci/changefile.sh
+++ travis-ci/changefile.sh
@@ -2,6 +2,7 @@
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 cd $TRAVIS_BUILD_DIR
+sudo chown (whoami):(whoami) -R $TRAVIS_BUILD_DIR
 sudo git remote -v
 sudo git remote rm origin
 sudo git config --global user.email "${GIT_EMAIL}"

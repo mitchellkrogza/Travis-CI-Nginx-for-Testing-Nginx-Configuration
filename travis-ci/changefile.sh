708a887916d73840471af1e62dc9c6a0514bf674
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 15:11:42 +0200

    check perms on travis .git folder

--- travis-ci/changefile.sh
+++ travis-ci/changefile.sh
@@ -13,6 +13,8 @@ echo "Second check of remote"
 sudo git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 sudo git remote -v
 echo "Third Check of Remote After Adding it"
+ls -l $TRAVIS_BUILD_DIR/.git/FETCH_HEAD
+echo "Check permissions on git HEAD"
 sudo git config --global user.email "${GIT_EMAIL}"
 sudo git config --global user.name "${GIT_NAME}"
 sudo git config --global push.default simple

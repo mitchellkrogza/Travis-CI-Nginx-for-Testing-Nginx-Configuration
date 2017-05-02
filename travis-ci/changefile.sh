2a637ea7dc8e79114aa97013583ef59e546aa507
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 15:33:05 +0200

    remove sudo from git commands

--- travis-ci/changefile.sh
+++ travis-ci/changefile.sh
@@ -4,32 +4,32 @@ MONTH=$(date +"%m")
 cd $TRAVIS_BUILD_DIR
 #sudo whoami
 #sudo chown root:root -R $TRAVIS_BUILD_DIR
-sudo git remote -v
+git remote -v
 echo "First check of remote"
-sudo git remote rm origin
+git remote rm origin
 echo "Try removing remote"
-sudo git remote -v
+git remote -v
 echo "Second check of remote"
-sudo git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-sudo git remote -v
+git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git remote -v
 echo "Third Check of Remote After Adding it"
 ls -l $TRAVIS_BUILD_DIR/.git/FETCH_HEAD
 ls -la $TRAVIS_BUILD_DIR
 ls -la $TRAVIS_BUILD_DIR/.git
-sudo git fetch origin master
+git fetch origin master
 ls -la $TRAVIS_BUILD_DIR/.git
 echo "Check permissions on git HEAD / get dir listing of travis build dir"
-sudo git config --global user.email "${GIT_EMAIL}"
-sudo git config --global user.name "${GIT_NAME}"
-sudo git config --global push.default simple
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global push.default simple
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 #echo "$msg" >> $TRAVIS_BUILD_DIR/build.txt
 sudo $TRAVIS_BUILD_DIR/travis-ci/modify-globalblacklist.sh
 #sudo git add $TRAVIS_BUILD_DIR/globalblacklist.conf
-sudo git add -A .
+git add -A .
 #git add $TRAVIS_BUILD_DIR/build.txt
-sudo git commit -m "Update build version file with $TRAVIS_BUILD_NUMBER"
+git commit -m "Update build version file with $TRAVIS_BUILD_NUMBER"
 
 #YEAR=$(date +"%Y")
 #MONTH=$(date +"%m")
--- travis-ci/deploy2.sh
+++ travis-ci/deploy2.sh
@@ -2,16 +2,16 @@
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 cd $TRAVIS_BUILD_DIR
-sudo git config --global user.email "${GIT_EMAIL}"
-sudo git config --global user.name "${GIT_NAME}"
-sudo git config --global push.default simple
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global push.default simple
 #sudo git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
-sudo git fetch --tags
+git fetch --tags
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-if sudo git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
-sudo git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-sudo git push origin master && sudo git push origin master --tags
+if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
+git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
+git push origin master && git push origin master --tags
 ls -aR
 else echo Tag already exists!; fi
 

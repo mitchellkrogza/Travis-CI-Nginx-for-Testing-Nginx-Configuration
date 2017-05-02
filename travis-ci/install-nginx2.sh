390c8389f8ec313c51be06462a196e69ae6da64b
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 11:23:36 +0200

    chaneg deploy sequence

--- travis-ci/install-nginx2.sh
+++ travis-ci/install-nginx2.sh
@@ -59,7 +59,7 @@ set -x
 
 # Start nginx.
 #nginx -c "$DIR/nginx/nginx.conf"
-echo $TRAVIS_BUILD_DIR
+#echo $TRAVIS_BUILD_DIR
 sudo rm /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/install-ngxblocker -O /usr/sbin/install-ngxblocker
 sudo wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/setup-ngxblocker -O /usr/sbin/setup-ngxblocker
@@ -84,11 +84,19 @@ cd /usr/sbin
 sudo ./update-ngxblocker
 sudo service nginx reload
 sudo chmod +x $TRAVIS_BUILD_DIR/travis-ci/modify-globalblacklist.sh
-#sudo export GIT_TAG=v1.0.$TRAVIS_BUILD_NUMBER
 sudo $TRAVIS_BUILD_DIR/travis-ci/modify-globalblacklist.sh
 cd $TRAVIS_BUILD_DIR
 sudo git remote -v
 sudo git remote rm origin
 #cat $TRAVIS_BUILD_DIR/globalblacklist.conf
+sudo git config --global user.email "${GIT_EMAIL}"
+sudo git config --global user.name "${GIT_NAME}"
+sudo git config --global push.default simple
+sudo export GIT_TAG=v1.0.$TRAVIS_BUILD_NUMBER
+#msg="Build $TRAVIS_BUILD_NUMBER"
+#echo "$msg" >> $TRAVIS_BUILD_DIR/build.txt
+sudo git add $TRAVIS_BUILD_DIR/globalblacklist.conf
+sudo git commit -m "Build $TRAVIS_BUILD_NUMBER"
+
 #sudo sed --version
 #sudo apt-cache policy dash

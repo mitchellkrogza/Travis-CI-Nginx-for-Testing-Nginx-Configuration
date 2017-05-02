cd75f4fa45343db7378bcde6493caba1fcc2113b
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 13:43:43 +0200

    fix git remote’s issue

--- travis-ci/changefile.sh
+++ travis-ci/changefile.sh
@@ -3,9 +3,9 @@ YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 cd $TRAVIS_BUILD_DIR
 sudo whoami
-sudo chown root:root -R $TRAVIS_BUILD_DIR
-sudo git remote -v
-sudo git remote rm origin
+#sudo chown root:root -R $TRAVIS_BUILD_DIR
+#sudo git remote -v
+#sudo git remote rm origin
 sudo git config --global user.email "${GIT_EMAIL}"
 sudo git config --global user.name "${GIT_NAME}"
 sudo git config --global push.default simple

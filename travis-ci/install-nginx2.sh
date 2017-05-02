d3f39540175b65f877abf5580fe0c3b9c2b2f663
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-02 11:24:49 +0200

    set deploy.sh executable

--- travis-ci/install-nginx2.sh
+++ travis-ci/install-nginx2.sh
@@ -84,6 +84,7 @@ cd /usr/sbin
 sudo ./update-ngxblocker
 sudo service nginx reload
 sudo chmod +x $TRAVIS_BUILD_DIR/travis-ci/modify-globalblacklist.sh
+sudo chmod +x $TRAVIS_BUILD_DIR/travis-ci/deploy.sh
 sudo $TRAVIS_BUILD_DIR/travis-ci/modify-globalblacklist.sh
 cd $TRAVIS_BUILD_DIR
 sudo git remote -v

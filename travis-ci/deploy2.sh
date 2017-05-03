03fdb35b7e43d7259a60c3b3112715f7c98a3d67
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-03 12:31:31 +0200

    get git branch

--- travis-ci/deploy2.sh
+++ travis-ci/deploy2.sh
@@ -7,6 +7,7 @@ cd $TRAVIS_BUILD_DIR
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
+git branch
 #sudo git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 #git fetch --tags
 msg="Tag $GIT_TAG Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
@@ -18,7 +19,7 @@ git add $TRAVIS_BUILD_DIR/globalblacklist.conf
 #git add $TRAVIS_BUILD_DIR/build.txt
 export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 git tag $GIT_TAG -a -m "Tag $GIT_TAG Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git commit -am "Update build version file $GIT_TAG with $TRAVIS_BUILD_NUMBER"
+#git commit -am "Update build version file $GIT_TAG with $TRAVIS_BUILD_NUMBER"
 sudo git push origin master && git push origin master --tags
 #git push https://${GH_TOKEN}@github.com/mitchellkrogza/Travis-CI-Nginx-for-Testing-Nginx-Configuration --all
 ls -aR

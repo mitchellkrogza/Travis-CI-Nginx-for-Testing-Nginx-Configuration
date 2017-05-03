ab2e00bd6b4a3cc1c108baefec9e8d2f3c813969
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-03 12:40:31 +0200

    check working branch after doing git checkout $TRAVIS_BRANCH

--- travis-ci/deploy2.sh
+++ travis-ci/deploy2.sh
@@ -9,6 +9,7 @@ git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
 git branch
 git checkout $TRAVIS_BRANCH
+git branch
 #sudo git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 #git fetch --tags
 msg="Tag $GIT_TAG Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"

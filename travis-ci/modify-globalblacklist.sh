3971651edd7bcf70e2adc66a2110c699e86b7c8d
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-04 11:05:25 +0200

    change version information block of globalblacklist.conf

--- travis-ci/modify-globalblacklist.sh
+++ travis-ci/modify-globalblacklist.sh
@@ -5,7 +5,7 @@
 
 # Start time of script generation
 start=$(date +%s.%N)
-MY_GIT_TAG=v1.0.$TRAVIS_BUILD_NUMBER
+MY_GIT_TAG=v2.2017.07.$TRAVIS_BUILD_NUMBER
 
 # Temporary database files we create
 _inputdbA=/tmp/lastupdated.db
@@ -17,29 +17,31 @@ _tmpnginxA=tmpnginxA
 _tmpnginxB=tmpnginxB
 
 # Start and End Strings to Search for to do inserts into template
-_startdate="### Last Updated"
-_enddate="### End Last Updated"
-_startgen="### Generated in"
-_endgen="### End Generated in"
+_startmarker="### Version Information"
+_endmarker="### Version Information END"
+#_startgen="### Generated in"
+#_endgen="### End Generated in"
 
-# PRINT DATE AND TIME OF LAST UPDATE
+# PRINT VERSION INFORMATION
 # **********************************
 LASTUPDATEIFS=$IFS
 IFS=$'\n'
 now="$(date)"
-echo $_startdate >> $_tmpnginxA
-printf "### Updated: "$now"\n### Build: "$MY_GIT_TAG"\n" >> $_tmpnginxA
-echo $_enddate  >> $_tmpnginxA
+end=$(date +%s.%N)    
+echo $_startmarker >> $_tmpnginxA
+runtime=$(python -c "print(${end} - ${start})")
+printf "### Version: "$MY_GIT_TAG"\n### Updated: "$now"\n### Generated In: "$runtime" seconds\n" >> $_tmpnginxA
+echo $_endmarker  >> $_tmpnginxA
 IFS=$LASTUPDATEIFS
 mv $_tmpnginxA $_inputdbA
 ed -s $_inputdbA<<\IN
-1,/### Last Updated/d
-/### End Last Updated/,$d
+1,/### Version Information/d
+/### Version Information END/,$d
 ,d
 .r /home/travis/build/mitchellkrogza/Travis-CI-Nginx-for-Testing-Nginx-Configuration/globalblacklist.conf
-/### Last Updated/x
+/### Version Information/x
 .t.
-.,/### End Last Updated/-d
+.,/### Version Information END/-d
 #,p
 #,p used to print output replaced with w below to write
 w /home/travis/build/mitchellkrogza/Travis-CI-Nginx-for-Testing-Nginx-Configuration/globalblacklist.conf
@@ -49,26 +51,26 @@ rm $_inputdbA
 
 # End time of script generation
 # *****************************
-GENTIME=$IFS
-IFS=$'\n'
-end=$(date +%s.%N)    
-runtime=$(python -c "print(${end} - ${start})")
-echo $_startgen >> $_tmpnginxB
-printf "### "$runtime" seconds\n" >> $_tmpnginxB
-echo $_endgen  >> $_tmpnginxB
-IFS=$GENTIME
-mv $_tmpnginxB $_inputdbB
-ed -s $_inputdbB<<\IN
-1,/### Generated in/d
-/### End Generated in/,$d
-,d
-.r /home/travis/build/mitchellkrogza/Travis-CI-Nginx-for-Testing-Nginx-Configuration/globalblacklist.conf
-/### Generated in/x
-.t.
-.,/### End Generated in/-d
-w /home/travis/build/mitchellkrogza/Travis-CI-Nginx-for-Testing-Nginx-Configuration/globalblacklist.conf
-q
-IN
-rm $_inputdbB
+#GENTIME=$IFS
+#IFS=$'\n'
+#end=$(date +%s.%N)    
+#runtime=$(python -c "print(${end} - ${start})")
+#echo $_startgen >> $_tmpnginxB
+#printf "### "$runtime" seconds\n" >> $_tmpnginxB
+#echo $_endgen  >> $_tmpnginxB
+#IFS=$GENTIME
+#mv $_tmpnginxB $_inputdbB
+#ed -s $_inputdbB<<\IN
+#1,/### Generated in/d
+#/### End Generated in/,$d
+#,d
+#.r /home/travis/build/mitchellkrogza/Travis-CI-Nginx-for-Testing-Nginx-Configuration/globalblacklist.conf
+#/### Generated in/x
+#.t.
+#.,/### End Generated in/-d
+#w /home/travis/build/mitchellkrogza/Travis-CI-Nginx-for-Testing-Nginx-Configuration/globalblacklist.conf
+#q
+#IN
+#rm $_inputdbB
 
 exit 0

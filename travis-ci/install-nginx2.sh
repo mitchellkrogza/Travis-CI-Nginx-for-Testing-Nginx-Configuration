a5dcdaa9e96807b315d0d81811e42911af7b3fbb
Mitchell Krog <mitchellkrog@gmail.com>
2017-05-04 11:26:45 +0200

    set timezone in travis

--- travis-ci/install-nginx2.sh
+++ travis-ci/install-nginx2.sh
@@ -61,8 +61,9 @@ set -x
 #nginx -c "$DIR/nginx/nginx.conf"
 #echo $TRAVIS_BUILD_DIR
 # Set TimeZone
-sudo timedatectl set-timezone Africa/Johannesburg
+#sudo timedatectl set-timezone Africa/Johannesburg
 sudo timedatectl status
+date
 # End Set Timezone
 sudo rm /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/install-ngxblocker -O /usr/sbin/install-ngxblocker
